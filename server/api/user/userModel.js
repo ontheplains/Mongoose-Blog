284882802795263139c68641735346f198f421e4
Justin Caldwell <justinwritescode@gmail.com>
2016-07-07 19:50:55 -0600

    userModel.js built

--- server/api/user/userModel.js
+++ server/api/user/userModel.js
@@ -1,1 +1,17 @@
 // Create a user schema
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema ({
+    firstName: String,
+    lastName: String,
+    email: String,
+    accountCreated: Date,
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
